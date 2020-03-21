9ba01bd1c60cb1aed9085ddafcad5609e2bafef9
Drew Swinicki <dswinicki93@gmail.com>
2020-03-20 21:40:54 -0400

    updated variables for password

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+// prompt need password
+// // prompt user to input criteria
+// // password length 8-128
+// // lower case
+// // upper case
+// // special
+// // numbers
+// // generate password 
+// // display password
+
+
+
+var upperCaseL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCaseL = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specialChar = "'!@#$%^&*_+~`|\:;?><,./-='"
+
+
+
+var pLength = prompt("How many characters do you need your password to be between 8 and 128 characters?")
+// if (plength < 8 || plenght > 128) {
+//     prompt("Please enter valid repsonse.")
+// }
+
+console.log(pLength)
+1
+var upperPass = confirm("Do you require uppercase letters for your password?")
+console.log(upperPass)
+var lowerPass = confirm("Do you require lowercase letters for your password?")
+console.log(lowerPass)
+var numberPass = confirm("Do you require numbers for your password?")
+console.log(numberPass)
+var specialPass = confirm("Do you require special characters letters for your password?")
+console.log(specialPass)
+
+
+if (upperPass === true ){
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
